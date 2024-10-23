52d86480f9049dbaac82f178dc1b8ce9efa5936f
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-10-23 19:45:21 +0700

    fix bug

--- algo/modeling/expand.py
+++ algo/modeling/expand.py
@@ -8,5 +8,9 @@ def expand(problem, node):
         s_prime = problem.result(s, action)
         # Calculate the cost of moving to the new state
         cost = node.path_cost + problem.action_cost(s, action)
+        # Calculate the steps
+        steps = node.num_steps + 1
+        # Calculate the weight
+        weight = node.total_weight + problem.action_cost(s, action) - 1
         # Yield a new node corresponding to the resulting state
-        yield Node(state=s_prime, parent=node, action=action, path_cost=cost)
+        yield Node(state=s_prime, parent=node, action=action, path_cost=cost, num_steps=steps, total_weight=weight)
--- algo/modeling/node.py
+++ algo/modeling/node.py
@@ -1,9 +1,15 @@
 class Node:
-    def __init__(self, state, parent=None, action=None, path_cost=0):
+    def __init__(self, state, parent=None, action=None, path_cost=0, num_steps=0, total_weight=0):
         self.state = state  # The state to which the node corresponds
         self.parent = parent  # The parent node that generated this node
         self.action = action  # The action taken to generate this node
+        self.num_steps = num_steps  # The number of steps Ares has to take
+        self.total_weight = total_weight  # The total weight Ares has to carry
         self.path_cost = path_cost  # The cost from the initial state to this node
+
+    def __lt__(self, other):
+        """Compares two nodes based on path cost (for priority queues)."""
+        return self.path_cost < other.path_cost
 
     def __repr__(self):
         return f"Node(state={self.state}, parent={self.parent}, action={self.action}, path_cost={self.path_cost})"
