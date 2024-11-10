6ab85ea79e2f7f50d45557bf6d55d9d054ed2096
Mmyxxan <127069162+Mmyxxan@users.noreply.github.com>
2024-11-10 18:04:35 +0700

    fix

--- algo/generate_output.py
+++
@@ -1,3 +0,0 @@
-from io_process import generate_output_for_input_file
-
-generate_output_for_input_file("../input/input-05.txt")
---
+++ generate_output.py
@@ -0,0 +1,3 @@
+from algo.io_process import generate_output_for_input_file
+
+generate_output_for_input_file("input/input-01.txt")
